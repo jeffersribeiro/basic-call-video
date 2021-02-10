8b5da080a29be75a432c5fc88e81dc27c0c5d73e
jeffersribeiro <jefferson_ribeiro12345@outlook.com>
2021-02-10 18:08:59 -0300

    first commit

--- src/components/Call.js
+++ src/components/Call.js
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
-import AgoraRTC from "agora-rtc-sdk";
-let client = AgoraRTC.createClient({ mode: "live", codec: "h264" });
+import React, { Component } from 'react';
+import AgoraRTC from 'agora-rtc-sdk';
+let client = AgoraRTC.createClient({ mode: 'live', codec: 'h264' });
 
 const USER_ID = Math.floor(Math.random() * 1000000001);
-const APP_ID = "aed1787f4f1d41a0baf205dc0193f586";
+const APP_ID = 'aed1787f4f1d41a0baf205dc0193f586';
 
 export default class Call extends Component {
   localStream = AgoraRTC.createStream({
     streamID: USER_ID,
     audio: true,
     video: true,
-    screen: false
+    screen: false,
   });
 
   state = {
-    remoteStreams: []
+    remoteStreams: [],
   };
 
   componentDidMount() {
@@ -23,7 +23,7 @@ export default class Call extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if (prevProps.channel !== this.props.channel && this.props.channel !== "") {
+    if (prevProps.channel !== this.props.channel && this.props.channel !== '') {
       this.joinChannel();
     }
   }
@@ -32,11 +32,11 @@ export default class Call extends Component {
     let me = this;
     me.localStream.init(
       function() {
-        console.log("getUserMedia successfully");
-        me.localStream.play("agora_local");
+        console.log('getUserMedia successfully');
+        me.localStream.play('agora_local');
       },
       function(err) {
-        console.log("getUserMedia failed", err);
+        console.log('getUserMedia failed', err);
       }
     );
   };
@@ -45,10 +45,10 @@ export default class Call extends Component {
     client.init(
       APP_ID,
       function() {
-        console.log("AgoraRTC client initialized");
+        console.log('AgoraRTC client initialized');
       },
       function(err) {
-        console.log("AgoraRTC client init failed", err);
+        console.log('AgoraRTC client init failed', err);
       }
     );
     this.subscribeToClient();
@@ -56,30 +56,30 @@ export default class Call extends Component {
 
   subscribeToClient = () => {
     let me = this;
-    client.on("stream-added", me.onStreamAdded);
-    client.on("stream-subscribed", me.onRemoteClientAdded);
+    client.on('stream-added', me.onStreamAdded);
+    client.on('stream-subscribed', me.onRemoteClientAdded);
 
-    client.on("stream-removed", me.onStreamRemoved);
+    client.on('stream-removed', me.onStreamRemoved);
 
-    client.on("peer-leave", me.onPeerLeave);
+    client.on('peer-leave', me.onPeerLeave);
   };
 
-  onStreamAdded = evt => {
+  onStreamAdded = (evt) => {
     let me = this;
     let stream = evt.stream;
-    console.log("New stream added: " + stream.getId());
+    console.log('New stream added: ' + stream.getId());
     me.setState(
       {
         remoteStreams: {
           ...me.state.remoteStream,
-          [stream.getId()]: stream
-        }
+          [stream.getId()]: stream,
+        },
       },
       () => {
         // Subscribe after new remoteStreams state set to make sure
         // new stream dom el has been rendered for agora.io sdk to pick up
         client.subscribe(stream, function(err) {
-          console.log("Subscribe stream failed", err);
+          console.log('Subscribe stream failed', err);
         });
       }
     );
@@ -92,30 +92,30 @@ export default class Call extends Component {
       me.props.channel,
       USER_ID,
       function(uid) {
-        console.log("User " + uid + " join channel successfully");
+        console.log('User ' + uid + ' join channel successfully');
         client.publish(me.localStream, function(err) {
-          console.log("Publish local stream error: " + err);
+          console.log('Publish local stream error: ' + err);
         });
 
-        client.on("stream-published", function(evt) {
-          console.log("Publish local stream successfully");
+        client.on('stream-published', function(evt) {
+          console.log('Publish local stream successfully');
         });
       },
       function(err) {
-        console.log("Join channel failed", err);
+        console.log('Join channel failed', err);
       }
     );
   };
 
-  onRemoteClientAdded = evt => {
+  onRemoteClientAdded = (evt) => {
     let me = this;
     let remoteStream = evt.stream;
     me.state.remoteStreams[remoteStream.getId()].play(
-      "agora_remote " + remoteStream.getId()
+      'agora_remote ' + remoteStream.getId()
     );
   };
 
-  onStreamRemoved = evt => {
+  onStreamRemoved = (evt) => {
     let me = this;
     let stream = evt.stream;
     if (stream) {
@@ -127,11 +127,11 @@ export default class Call extends Component {
 
       me.setState({ remoteStreams });
 
-      console.log("Remote stream is removed " + stream.getId());
+      console.log('Remote stream is removed ' + stream.getId());
     }
   };
 
-  onPeerLeave = evt => {
+  onPeerLeave = (evt) => {
     let me = this;
     let stream = evt.stream;
     if (stream) {
@@ -143,22 +143,22 @@ export default class Call extends Component {
 
       me.setState({ remoteStreams });
 
-      console.log(evt.uid + " leaved from this channel");
+      console.log(evt.uid + ' leaved from this channel');
     }
   };
 
   render() {
     return (
       <div>
-        <div id="agora_local" style={{ width: "400px", height: "400px" }} />
-        {Object.keys(this.state.remoteStreams).map(key => {
+        <div id='agora_local' style={{ width: '400px', height: '400px' }} />
+        {Object.keys(this.state.remoteStreams).map((key) => {
           let stream = this.state.remoteStreams[key];
           let streamId = stream.getId();
           return (
             <div
               key={streamId}
               id={`agora_remote ${streamId}`}
-              style={{ width: "400px", height: "400px" }}
+              style={{ width: '400px', height: '400px' }}
             />
           );
         })}
